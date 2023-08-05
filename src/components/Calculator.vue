c79db2a25c9d3d2ea6fad98606193450a12cbd3a
Monika Rasała <mrasala583@gmail.com>
2023-08-05 23:17:50 +0200

    providing basic functionality part2

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="Hello"></div>
-    <h1>{{msg}}</h1>
+    <h1 class='text-center'>{{msg}}</h1>
 
     <div class="col-md-4 m-3">
         <table class="table table-bordered text-center ">
@@ -14,30 +14,31 @@
       <td v-on:click="clearField">C</td>
       <td v-on:click="setNagativerOrPosiive">+/-</td>
       <td v-on:click="calculatePercenage">%</td>
-      <td class="lastColumn"><svg width="10px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M272 96a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zm0 320a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM400 288c17.7 0 32-14.3 32-32s-14.3-32-32-32H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H400z"/></svg></td>
+      <td class="lastColumn"><svg width="10px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path fill="#fff" d="M272 96a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zm0 320a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM400 288c17.7 0 32-14.3 32-32s-14.3-32-32-32H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H400z"/></svg></td>
     </tr>
     <tr>
-        <td>7</td>
-        <td>8</td>
-        <td>9</td>
+        <td v-on:click="getNumber(7)">7</td>
+        <td v-on:click="getNumber(8)">8</td>
+        <td v-on:click="getNumber(9)">9</td>
         <td class="lastColumn">x</td>
     </tr>
     <tr>
-        <td>4</td>
-        <td>5</td>
-        <td>6</td>
+        <td v-on:click="getNumber(4)">4</td>
+        <td v-on:click="getNumber(5)">5</td>
+        <td v-on:click="getNumber(6)">6</td>
         <td class="lastColumn">-</td>
     </tr>
     <tr>
-        <td>1</td>
-        <td>2</td>
-        <td>3</td>
-        <td class="lastColumn">+</td>
+        <td v-on:click="getNumber(1)">1</td>
+        <td v-on:click="getNumber(2)">2</td>
+        <td v-on:click="getNumber(3)">3</td>
+        <td class="lastColumn" 
+        @click="processOutput">+</td>
     </tr>
     <tr>
-        <td colspan="2">0</td>
-        <td>.</td>
-        <td>=</td>
+        <td colspan="2" v-on:click="getNumber(0)">0</td>
+        <td  v-on:click="getDok()">.</td>
+        <td class="lastColumn">=</td>
     </tr>    
   </tbody>
 </table>
@@ -53,18 +54,34 @@ export default {
     }, 
     data(){
         return{
-            output: '13'
+            output: ""
         }
     }, 
     methods: {
         clearField() {
-            this.output = '0';
+            this.output = 0;
         }, 
         setNagativerOrPosiive(){
-            this.output = this.output[0] === '-' ? this.output.slice(1) : '${this.output}';
+            this.output = this.output[0] === '-' ? this.output.slice(1) : `-${this.output}`;
         }, 
         calculatePercenage(){
             this.output = parseFloat(this.output)/100; 
+        }, 
+        getNumber(number){
+             this.output = `${this.output}${number}`
+        }, 
+        getDot(){
+            if(this.output.indexOf('.') === -1){
+                this.output = this.output+'.';
+            }
+        }, 
+        processOutput(){
+            this.operation = (a,b) => {
+            return parseFloat(a) + parseFloat(b) 
+            }
+        }, 
+        updateOutput(){
+            this.operation = ``
         }
     }
 }
