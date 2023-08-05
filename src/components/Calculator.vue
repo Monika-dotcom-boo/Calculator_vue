2bd4a12f2cabe82f810232043cc25e6b398feda6
Monika Rasała <mrasala583@gmail.com>
2023-08-05 22:32:59 +0200

    providing basic functionality

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -3,12 +3,17 @@
     <h1>{{msg}}</h1>
 
     <div class="col-md-4 m-3">
-        <table class="table table-bordered">
+        <table class="table table-bordered text-center ">
+        <tr class="output">
+            <td colspan="4">
+                {{output || 0 }}
+            </td>
+        </tr>
   <tbody>
     <tr>
-      <td>C</td>
-      <td>+/-</td>
-      <td>%</td>
+      <td v-on:click="clearField">C</td>
+      <td v-on:click="setNagativerOrPosiive">+/-</td>
+      <td v-on:click="calculatePercenage">%</td>
       <td class="lastColumn"><svg width="10px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M272 96a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zm0 320a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM400 288c17.7 0 32-14.3 32-32s-14.3-32-32-32H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H400z"/></svg></td>
     </tr>
     <tr>
@@ -45,19 +50,39 @@
 export default {
     props: {
         msg: String
+    }, 
+    data(){
+        return{
+            output: '13'
+        }
+    }, 
+    methods: {
+        clearField() {
+            this.output = '0';
+        }, 
+        setNagativerOrPosiive(){
+            this.output = this.output[0] === '-' ? this.output.slice(1) : '${this.output}';
+        }, 
+        calculatePercenage(){
+            this.output = parseFloat(this.output)/100; 
+        }
     }
 }
 
 </script>
 
 <style scoped>
+    .output{
+        background-color: #333;
+        color:#fff;
+    }
     .lastColumn{
         background-color: orange;
         color:#fff;
     }
-
     .lastColumn:active{
         background-color: #333;
         color:#fff;
     }
+
 </style>
